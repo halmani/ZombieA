df5639f2011a409f74d3be2e2cab65136daaa1ae
halmani <halmani@hotmail.com>
2016-07-14 22:24:31 +0900

    3Dアクションゲーム

---
+++ ZombieA/Assets/Scripts/InputController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class InputController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	private Vector2 startPos;
+	private Vector2 direction;
+
+	public Vector3 DragDirection2Dto3D
+	{
+		get { return new Vector3(direction.x, 0, direction.y); }
+	}
+
+	// 入力 ----------------------------------------
+	private PointerEventData lastEventData;
+	public void OnBeginDrag(PointerEventData data)
+	{
+		lastEventData = data;
+		startPos = data.pressPosition;
+	}
+
+	public void OnDrag(PointerEventData data)
+	{
+		lastEventData = data;
+		direction += data.delta;
+	}
+
+	public void OnEndDrag(PointerEventData data)
+	{
+		lastEventData = data;
+		startPos = Vector2.zero;
+		direction = Vector2.zero;
+	}
+}
---
+++ ZombieA/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	private void Start()
+	{
+		Debug.Log("player is me");
+	}
+	
+	private void Update()
+	{
+
+	}
+}
