41e666d7dcf378159d367ab2439a49ddda237bf9
halmani <halmani@hotmail.com>
2017-12-25 21:38:02 +0900

    ゲームマネージャ追加してみた

--- ZombieA/Assets/Scripts/Enemy.cs
+++ ZombieA/Assets/Scripts/Enemy.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Enemy : Character
 {
+	private static int count = 0;
+	public static int Count
+	{
+		get { return count; }
+	}
+
 	public int hp = 3;
 
 
+	private void Awake()
+	{
+		count++;
+	}
+
 	private void HitDamage(int damage)
 	{
-		if ((hp - damage) <= 0 )
+		if ((hp - damage) <= 0)
 		{
 			Destroy(this.gameObject);
 			hp = 0;
+			count--;
+			return;
 		}
 		hp -= damage;
 	}
